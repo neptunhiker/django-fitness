2eeaa4624ebca77ecb7a16f1ca81966eed8ace3d
neptunhiker <bastiusa47@yahoo.de>
2024-07-29 09:00:28 +0200

    Create a working version of login, signup, and logout functionality with a simple base.html incl. navbar

--- accounts/views.py
+++ accounts/views.py
@@ -8,4 +8,6 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+    
+
 
--- src/settings.py
+++ src/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"

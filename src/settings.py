6759ff24dca1c6fa6ce5c5bc148a78a1b7c1a7e5
neptunhiker <bastiusa47@yahoo.de>
2024-07-31 11:05:49 +0200

    Set allowed hosts and csrf trusted origins

--- src/settings.py
+++ src/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".fly.dev", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://ladywarcraft-fitness.fly.dev"]
 
 
 # Application definition

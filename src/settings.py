9f3f62813a3e957672241da4d90bd9f36fd080a0
neptunhiker <bastiusa47@yahoo.de>
2024-07-31 11:04:20 +0200

    Set Debug mode and secret key

--- src/settings.py
+++ src/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#^e7+)2c5y9v5v(r+ckhx)^o)me@5a32fq5hhodjmjp^fksck3"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 

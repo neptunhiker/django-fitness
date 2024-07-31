a1abcdf89414b30bc090e419c4fe151a131887ff
neptunhiker <bastiusa47@yahoo.de>
2024-07-31 10:57:25 +0200

    Configure database for deployment

--- src/settings.py
+++ src/settings.py
@@ -86,10 +86,7 @@ WSGI_APPLICATION = "src.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.dj_db_url("DATABASE_URL", default="sqlite:///db.sqlite3")
 }
 
 
